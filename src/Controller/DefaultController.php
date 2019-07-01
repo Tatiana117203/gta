742c685aeb540b83250d135db347b8b89908ca40
Tatiana <tatiana117203@gmail.com>
2019-07-01 21:04:33 +0300

    new

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,11 +14,14 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      */
-    public function index() {
-        $message = 'I\'m index method';
+    public function index (ProductRepository $productRepository) {
+        $products = $productRepository->findAll(
+        ['isTop' => true],
+        ['name' => 'ASC']
+        ) ;
         return $this->render('default/index.html.twig' ,
         [
-            'mess' => $message,
+            'products' => $products,
         ]);
     }
 }
